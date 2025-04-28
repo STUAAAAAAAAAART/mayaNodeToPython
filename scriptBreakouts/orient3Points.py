f646968396776ad6a4704beba2249f0e88bdf67c
STUAAAAAAAAAART <88794944+STUAAAAAAAAAART@users.noreply.github.com>
2025-04-28 16:11:44 +0800

    complete refactor of orient3Points.py
    using quaternions over raw matrices

--- scriptBreakouts/orient3Points.py
+++ scriptBreakouts/orient3Points.py
@@ -1,109 +1,67 @@
-import numpy as np # trig functions are all in radians
-from numpy.linalg import inv as mInv
-
-# maya world transform matrix, position-only
-
-p1 = np.matrix([ # world position of joint being oriented
-	[1,0,0,0],
-	[0,1,0,0],
-	[0,0,1,0],
-	[0.491209,1.995682,-0.88089,1]])
-
-p2 = np.matrix([ # world position of forward joint
-	[1,0,0,0],
-	[0,1,0,0],
-	[0,0,1,0],
-	[2.317401,2.199864,-1.670406,1]])
-
-p3 = np.matrix([ # third joint, existing on the smae triangle plane
-	[1,0,0,0],
-	[0,1,0,0],
-	[0,0,1,0],
-	[0,0,0,1]])
-
-
-
+import maya.cmds as mc
+import maya.api.OpenMaya as om2
 
 def orient3Point(position, forward, other):
-	# convert world transforms to points
-	pass
-	# maya worldMatrix tuple
+	try:
+		assert mc.nodeType(position) == "joint" # test if type is joint
+	except:
+		raise AssertionError(f"input object not a joint: {position}")
+	# ==== maths =============
+	p0 = om2.MVector( mc.getAttr(f"{position}.worldMatrix")[-4:-1] )
+	p1 = om2.MVector( mc.getAttr(f"{forward }.worldMatrix")[-4:-1] )
+	p2 = om2.MVector( mc.getAttr(f"{other   }.worldMatrix")[-4:-1] )
 
-	# np.array 4x4
-	p1 : np.array = position # 
-	p2 : np.array = forward
-	p3 : np.array = other
+	vFwd = (p1-p0).normal() # forward vector
+	vPln = (p2-p0).normal() # coplanar vector
+	vNrm = (vFwd ^ vPln).normal() # planar normal
 
-	# translate components
-	pt1= p1.A[-1] # array([x,y,z,w])
-	pt2= p2.A[-1]
-	pt3= p3.A[-1]
+	qFwd = om2.MQuaternion( om2.MVector(1,0,0), vFwd, 1) # initial forward vector
+	qFwdZ = om2.MQuaternion( om2.MVector(1,0,0), om2.MVector(0,0,1), 1) * qFwd # initial forward vector's z-axis
+	#	rotate z-axis by forward quaternion
 
-	pvA= pt3-pt1 # triangle side vectors
-	pvB= pt2-pt1
+	alignPlane = om2.MPlane().setPlane(vFwd, 0) # plane perpendicular to triangle z-axis
+	vNrm # just a reminder
+	vFwdZ = (om2.MVector(1,0,0).rotateBy(qFwdZ)).normal() # is definitely on the plane
+	# point on plane relative to space = space vector - rejection vector
+	# rejection vector = distance from plane * normal vector
+	planeNrm = (vNrm - (alignPlane.distanceToPoint(vNrm) * alignPlane.normal())).normal() # current normal
+	planeFwdZ = (vFwdZ - (alignPlane.distanceToPoint(vFwdZ) * alignPlane.normal())).normal() # correct normal
+	qAlign = om2.MQuaternion(planeFwdZ, planeNrm, 1) # realignment rotation adjustment
+	qOrient = qFwd * qAlign # coplanar joint orient
 
-	pNml = np.cross(pvA[:-1], pvB[:-1]) # [x,y,z]
-	pNml = np.resize(pNml, 4)
-	pJnt = pt1-pt3
-	pFwd = pt2-pt3
-	pJnt[-1] = 1.0 # reset W component
-	pFwd[-1] = 1.0 # same
-	pNml[-1] = 1.0 # same
+	getEuler = qOrient.asEulerRotation()
+	orient = [getEuler.x, getEuler.y, getEuler.z]
 
-	orient = [0,0,0] # xyz
+	angleUnit = mc.currentUnit(q=True, angle=True)
+	if angleUnit == 'deg': # result in radians, convert to scene units
+		for i in range(3):
+			orient[i] = om2.MAngle(orient[i],om2.MAngle.kRadians).asDegrees()
 
-	# Z orient
-	pEval = pFwd - pJnt # [x,y,z,w]
-	orient[2] = np.arctan(pEval[1] / pEval[0] ) # z <- y/x
-	matZ = np.identity(4)
-	cosZ = np.cos(orient[2])
-	sinZ = np.sin(orient[2])
-	matZ[0,0] = matZ[1,1] = cosZ
-	matZ[0,1] = sinZ
-	matZ[1,0] = -sinZ
-#	matZ = matZ.transpose()
-	invZ = mInv(matZ)
-
-	pJnt = pJnt@invZ
-	pFwd = pFwd@invZ
-	pNml = pNml@invZ
-
-	# Y orient
-	pEval = pFwd - pJnt # [x,y,z,w]
-	orient[1] = np.arctan(-pEval[2] / pEval[0] ) # y <- z/x
-	# also not sure why ^this has to be negative? 
-	matY = np.identity(4)
-	cosY = np.cos(orient[1])
-	sinY = np.sin(orient[1])
-	matY[0,0] = matY[2,2] = cosY
-	matY[2,0] = sinY
-	matY[0,2] = -sinY
-#	matY = matY.transpose()
-	invY = mInv(matY)
-
-	pJnt = pJnt@invY
-	pFwd = pFwd@invY
-	pNml = pNml@invY
-
-	# X orient
-	pEval = pJnt # [x,y,z,w]
-	orient[0] = np.arctan(pEval[2] / pEval[1] ) # x <- y/z
-	matX = np.identity(4)
-	cosX = np.cos(orient[0])
-	sinX = np.sin(orient[0])
-	matX[1,1] = matX[2,2] = cosX
-	matX[1,2] = sinX
-	matX[2,1] = -sinX
-#	matX = matX.transpose()
-
-	orientMatrix = matX@matY@matZ
+	# ==== parent handler =============
+	getParentName = mc.listRelatives(position, p=True)
+	if getParentName: # if not None, contains [string]
+		getParentName = getParentName[0] # unpack string from list
+	getChildrenNames = mc.listRelatives(position, p=True) or [] # just relative names, two nodes with a common parent can not have the same name
+	holdSelf : om2.MSelectionList = om2.MSelectionList().add(position) # true pointer to object, not a bare string of temporary name
+	mslChildren : om2.MSelectionList = om2.MSelectionList()
+	for node in getChildrenNames:
+		mslChildren.add(node)
 	
-	for i in range(3):
-		orient[i] = float(np.rad2deg(orient[i]))
+	# ==== run =============
+	if getChildrenNames: # if not []
+		mc.parent(*getChildrenNames, w=True) # unparent children
+	if getParentName: # if not None
+		mc.parent(position, w=True) # unparent from parent
+		
+	mc.joint(position, e=True, o=orient) # apply orient of this joint
+
+	if getParentName: # if not None
+		mc.parent(holdSelf.getSelectionStrings(0), getParentName) # reparent to parent
+		mc.rename(holdSelf.getSelectionStrings(0), position) # reassert name
+	if getChildrenNames: # if not []
+		getString = mslChildren.getSelectionStrings()	 
+		mc.parent(*getString, position) # reparent children
+		for i in range(len(getChildrenNames)): # reassert names
+			mc.rename(getString[i],getChildrenNames[i])
 	
-	print(orient)
-	return orientMatrix
-
-testMatrix = orient3Point(p1,p2,p3)
-
-print(testMatrix)
+	return orient
